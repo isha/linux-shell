4b9b6f72205691d97fa68320bc63727b468be852
isha-l <isha_garg_92@yahoo.com>
2013-03-04 17:12:17 -0800

    Added help again!

--- Shell.c
+++ Shell.c
@@ -177,7 +177,32 @@ usage: dir
 			printf("\n");
 			free(namelist);
 		}
-	}	
+	}
+
+	/* PRINT HELP
+	usage: help
+	*/
+                        else if (strcmp(cmdArray[0],"help") == 0){
+
+				printf("\nhelp");
+				printf("\n\ndir\t\t:\tList all files in directory");
+				printf("\nUsage\t\t:\tdir\n");
+                                printf("\n\ncd\t\t:\tChange present working directory");
+                                printf("\nUsage\t\t:\tcd [path to new directory]\n");
+                                printf("\n\ngetenv\t\t:\tShows the value of the specified Environment Variable");
+                                printf("\nUsage\t\t:\tgetenv [name]\n");
+                                printf("\n\nsetenv\t\t:\tSets the value of an Environment Variable");
+                                printf("\nUsage\t\t:\tsetenv [name] [value]\n");
+                                printf("\n\nunsetenv\t:\tDelete an Environment Variable");
+                                printf("\nUsage\t\t:\tunsetenv [name]\n");
+                                printf("\n\nrun\t\t:\tRun an executable");
+                                printf("\nUsage\t\t:\trun [executable] [arguments]\n");
+
+
+
+                        }
+
+	
 
 /* EXECUTE COMMAND
 usage: [command] { arguments }
